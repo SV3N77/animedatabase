23981b8a2ab72decd8f691aa39837939b53b1890
SV3N77 <danny.jiang1234@hotmail.com>
2022-08-30 14:28:48 +1000

    add search with usequery

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,18 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <div className="grow bg-neutral-100">
-        <Component {...pageProps} />
+        <QueryClientProvider client={client}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
       </div>
       <Footer />
     </div>
