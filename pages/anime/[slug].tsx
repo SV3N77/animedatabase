03025481cc9f086351cc4700be3c3a1898b7a6be
SV3N77 <danny.jiang1234@hotmail.com>
2022-09-04 11:38:50 +1000

    update anime page

--- pages/anime/[slug].tsx
+++ pages/anime/[slug].tsx
@@ -1,12 +1,15 @@
-import { AnimeQuery } from "../../utils/AnimeQuery";
-import Image from "next/future/image";
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import Image from "next/future/image";
+import { AnimeQuery } from "../../utils/AnimeQuery";
 
 async function getAnime(slug: string) {
-  const URL = `https://kitsu.io/api/edge/anime?filter[slug]=${slug}`;
-  const { data } = await fetch(URL).then((res) => res.json());
-  return data[0] as AnimeQuery | undefined;
+  const URL = `https://kitsu.io/api/edge/anime?filter[slug]=${slug}&include=genres`;
+  const { data, included } = await fetch(URL).then((res) => res.json());
+  let results = data[0] as AnimeQuery;
+  results.genresArray = included;
+  return results as AnimeQuery;
 }
+
 export function getStaticPaths() {
   return {
     paths: [],
@@ -17,6 +20,7 @@ export function getStaticPaths() {
 export async function getStaticProps(ctx: GetStaticPropsContext) {
   const slug = ctx.params!.slug as string;
   const anime = await getAnime(slug);
+
   if (!anime) {
     return {
       notFound: true,
@@ -26,6 +30,7 @@ export async function getStaticProps(ctx: GetStaticPropsContext) {
     props: { anime },
   };
 }
+
 export default function index({
   anime,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
@@ -33,10 +38,10 @@ export default function index({
     <section className="container mx-auto mb-8">
       <div className="flex flex-col gap-4">
         <Image
-          className="aspect-[16/3]"
+          className="aspect-[18/3]"
           src={anime.attributes.coverImage.large}
           width={1536}
-          height={288}
+          height={256}
           priority
         />
         <div className="flex gap-4">
@@ -48,8 +53,10 @@ export default function index({
               height={380}
               priority
             />
-            <div className="text-2xl">{anime.attributes.canonicalTitle}</div>
             <div className="rounded-sm bg-slate-50 p-2 shadow-md">
+              <div className="mb-4 text-2xl">
+                {anime.attributes.canonicalTitle}
+              </div>
               <div className="text-xl">Anime Details</div>
               <div className="flex flex-col gap-2 text-base">
                 <div className="flex justify-between">
@@ -64,10 +71,44 @@ export default function index({
                   <div className="">Japranese (Romaji)</div>
                   <div>{anime.attributes.titles.ja_jp}</div>
                 </div>
+                <div className="flex justify-between">
+                  <div className="">Type</div>
+                  <div>{anime.attributes.subtype}</div>
+                </div>
+                <div className="flex justify-between">
+                  <div className="">Episodes</div>
+                  <div>{anime.attributes.episodeCount}</div>
+                </div>
+                <div className="flex justify-between">
+                  <div className="">Status</div>
+                  <div>{anime.attributes.status}</div>
+                </div>
+                <div className="flex justify-between">
+                  <div className="">Aired</div>
+                  <div>
+                    {anime.attributes.startDate} to
+                    {anime.attributes.endDate}
+                  </div>
+                </div>
+                {anime.attributes.ageRatingGuide ? (
+                  <div className="flex justify-between">
+                    <div className="">Rating</div>
+                    <div>{anime.attributes.ageRatingGuide}</div>
+                  </div>
+                ) : null}
               </div>
             </div>
           </div>
-          <div className="text-base">{anime.attributes.description}</div>
+          <div className="flex flex-col gap-2">
+            <div className="text-base">{anime.attributes.description}</div>
+            <div className="flex gap-4">
+              {anime.genresArray.map((genre) => (
+                <div key={genre.id} className="p-1">
+                  {genre.attributes.name}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- utils/AnimeQuery.ts
+++ utils/AnimeQuery.ts
@@ -87,4 +87,118 @@ export type AnimeQuery = {
     showType: string;
     nsfw: boolean;
   };
+  relationships: {
+    genres: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    categories: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    castings: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    installments: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    mappings: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    reviews: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    mediaRelationships: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    characters: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    staff: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    productions: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    quotes: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    episodes: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    streamingLinks: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    animeProductions: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    animeCharacters: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+    animeStaff: {
+      links: {
+        self: string;
+        related: string;
+      };
+    };
+  };
+  genresArray: GenresArray[];
+};
+
+export type GenresArray = {
+  id: string;
+  type: string;
+  links: {
+    self: string;
+  };
+  attributes: {
+    createdAt: string;
+    updatedAt: string;
+    name: string;
+    slug: string;
+    description?: string;
+  };
 };
