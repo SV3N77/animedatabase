869508c09c36401cdfcd0a17d882d44e3bc1d131
SV3N77 <danny.jiang1234@hotmail.com>
2022-09-14 17:10:52 +1000

    add load more to manga characters page

--- pages/manga/[slug]/characters.tsx
+++ pages/manga/[slug]/characters.tsx
@@ -1,5 +1,6 @@
 import { GetStaticPropsContext } from "next";
-import { Character } from "../../../utils/AnimeQuery";
+import { useState } from "react";
+import { Character, MoreCharacters } from "../../../utils/AnimeQuery";
 import { MangaQuery } from "../../../utils/MangaQuery";
 
 type CharacterProps = { manga: MangaQuery; mangaCharacters: Character[] };
@@ -14,6 +15,13 @@ async function getAllCharacters(id: string) {
   const URL = `https://kitsu.io/api/edge/castings?filter[media_type]=Manga&filter[media_id]=${id}&filter[is_character]=true&filter[language]=Japanese&include=character,person&page[limit]=20&sort=-featured`;
   const { included } = await fetch(URL).then((res) => res.json());
   return included as Character[];
+}
+
+async function getMoreCharacters(id: string, page: number) {
+  const URL = `https://kitsu.io/api/edge/castings?filter[media_type]=Manga&filter[media_id]=${id}&filter[is_character]=true&filter[language]=Japanese&include=character&page[limit]=10&page[offset]=${page}&sort=-featured`;
+  const { included, links } = await fetch(URL).then((res) => res.json());
+  let results = { characters: included, links: links } as MoreCharacters;
+  return results;
 }
 
 export function getStaticPaths() {
@@ -43,6 +51,33 @@ export async function getStaticProps(ctx: GetStaticPropsContext) {
   };
 }
 export default function characters({ manga, mangaCharacters }: CharacterProps) {
+  const [characters, setCharacters] = useState<Character[]>(mangaCharacters);
+  const [isNextPage, setIsNextPage] = useState(true);
+  const [page, setPage] = useState(10);
+
+  async function loadMore() {
+    const nextLoad = await getMoreCharacters(manga.id, page);
+    if (nextLoad.links.next) {
+      setPage((prev) => prev + 10);
+      setCharacters((prev) => {
+        const spreadArray = [...prev, ...nextLoad.characters];
+        // deDuped = [...new Map(arr.map(v => [v.id, v])).values()]
+        // creates a new map where Object.values(spreadArray).map((character) => [character.id, character,])).values()
+        // returns a new object array with no duplicated objects
+        const deDuped = [
+          ...new Map(
+            Object.values(spreadArray).map((character) => [
+              character.id,
+              character,
+            ])
+          ).values(),
+        ];
+        return deDuped;
+      });
+    } else {
+      setIsNextPage(false);
+    }
+  }
   return (
     <section className="container mx-auto py-8">
       <div className="flex flex-col gap-2">
@@ -50,7 +85,7 @@ export default function characters({ manga, mangaCharacters }: CharacterProps) {
           {manga.attributes.canonicalTitle} Characters
         </h1>
         <div className="flex flex-wrap gap-6">
-          {mangaCharacters.map((character) => (
+          {characters.map((character) => (
             <div key={character.id} className="w-56">
               <img
                 className="aspect-[3/4] w-full"
@@ -60,6 +95,7 @@ export default function characters({ manga, mangaCharacters }: CharacterProps) {
             </div>
           ))}
         </div>
+        {isNextPage && <button onClick={loadMore}>load more</button>}
       </div>
     </section>
   );
