6ffc654d42382235119a9afe4eaac394e72eb307
SV3N77 <danny.jiang1234@hotmail.com>
2022-09-21 11:48:24 +1000

    add react query useinfinitequery to search page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,59 @@
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import Head from "next/head";
 import Link from "next/link";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import { AnimeQuery, Links } from "../utils/AnimeQuery";
 
 type QueryProps = {
   anime: AnimeQuery[];
-  next: string;
+  next?: number;
 };
 
 // passing query string to get query data from api
-async function getAnimes(query: string) {
-  const URL = `https://kitsu.io/api/edge/anime?filter[text]=${query}&page[limit]=20`;
-  const { data } = await fetch(URL).then((res) => res.json());
-  return data as AnimeQuery[];
+async function getAnimes(query: string, pageParam: number) {
+  const url = `https://kitsu.io/api/edge/anime?filter[text]=${query}&page[limit]=10&page[offset]=${pageParam}`;
+  const { data, links } = await fetch(url).then((res) => res.json());
+  const nextPageURL = links.next ? new URL(links.next) : undefined;
+
+  let results = {
+    anime: data,
+    next: nextPageURL
+      ? nextPageURL.searchParams.get("page[offset]")
+      : undefined,
+  } as QueryProps;
+
+  return results;
 }
 
-async function getMoreAnime(query: string) {
-  const URL = `https://kitsu.io/api/edge/anime?filter[text]=${query}&page[limit]=20&page[offset]=0`;
-  const { data, links } = await fetch(URL).then((res) => res.json());
-  let results = { anime: data, next: links.next } as QueryProps;
+async function getMoreAnime(pageParam: number) {
+  const URL = `https://kitsu.io/api/edge/anime?filter[text]=${query}&page[limit]=10`;
+  const { data } = await fetch(URL).then((res) => res.json());
+  let results = { anime: data, next: pageParam + 10 } as QueryProps;
   return results;
 }
 
 function Home() {
   const [query, setQuery] = useState<string>("");
+  const { ref, inView } = useInView();
 
-  // useQuery with query
-  const search = useQuery({
-    queryKey: ["search", query],
-    queryFn: () => getAnimes(query),
-    enabled: query.length > 3,
-    keepPreviousData: true,
-  });
+  const { data, isFetching, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    ["search", query],
+    ({ pageParam = 0 }) => getAnimes(query, pageParam),
+    {
+      getNextPageParam: (lastPage) => {
+        return lastPage.next || undefined;
+      },
+      keepPreviousData: true,
+      enabled: query.length > 3,
+    }
+  );
 
-  // const searchs = useInfiniteQuery(
-  //   ["search", query],
-  //   () => getMoreAnime(query),
-  //   { getNextPageParam: (lastPage) => lastPage.next }
-  // );
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
 
   // form submit
   function onSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -56,7 +71,26 @@ function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="container mx-auto flex flex-col gap-4 py-8">
-        <h1 className="text-2xl">Search Anime</h1>
+        <h1 className="text-2xl">
+          Search Anime{" "}
+          {isFetching && (
+            <svg
+              className="ml-2 inline h-6 w-6 animate-spin fill-green-500 text-gray-200 dark:text-gray-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+          )}
+        </h1>
         <form onSubmit={onSubmit}>
           <label
             htmlFor="query"
@@ -96,44 +130,29 @@ function Home() {
             </button>
           </div>
         </form>
-        {search.isFetching && search.isPreviousData && (
-          <div className="text-center">
-            <div role="status">
-              <svg
-                className="mr-2 inline h-8 w-8 animate-spin fill-green-500 text-gray-200 dark:text-gray-600"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                  fill="currentFill"
-                />
-              </svg>
-              <span className="sr-only">Loading...</span>
-            </div>
-          </div>
-        )}
-        {search.data?.length === 0 && (
+
+        {data?.pages.at(0)?.anime.length === 0 && (
           <div className="text-center">{query} Not Found</div>
         )}
 
-        <section className="mt-4 flex flex-col gap-4 lg:grid lg:grid-cols-2">
-          {search.data?.map((attribute) => (
-            <AnimeCard key={attribute.id} anime={attribute} />
-          ))}
-          {/* {searchs.data?.pages.map((group, i) => (
-            <div>
-              {group.anime.map((ani) => (
-                <div>{ani.attributes.canonicalTitle}</div>
+        <section className="mt-4 flex flex-col gap-4 py-4 lg:grid lg:grid-cols-2">
+          {data?.pages?.map((animes, i) => (
+            <React.Fragment key={i}>
+              {animes.anime.map((ani) => (
+                <AnimeCard key={ani.id} anime={ani} />
               ))}
-            </div>
-          ))} */}
+            </React.Fragment>
+          ))}
         </section>
+        {hasNextPage && (
+          <button
+            ref={ref}
+            onClick={() => fetchNextPage()}
+            className="invisible"
+          >
+            load more
+          </button>
+        )}
       </section>
     </div>
   );
